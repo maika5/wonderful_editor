789386c7b9803abef346d3dccdee66a51f952180
和知舞香 <revolution.w88@gmail.com>
2023-12-05 22:33:20 +0900

    articleモデル作成 bundle exec rails g model article title:string body:textコマンド実行

---
+++ app/models/article.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+class Article < ApplicationRecord
+  # Articleは一つのUserに所属しています(Article belongs to a User.) 
+  belongs_to :user
+
+  # ArticleはたくさんのCommentを持っています(Article has many Comments.)
+  has_many :Comments
+
+  # ArticleはたくさんのCommentを持っています(Article has many article_likes.)
+  has_many :article_likes
+end
