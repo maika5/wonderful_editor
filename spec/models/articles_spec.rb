d1685392040fccbe14a2abb3a4a20c312e68a3e4
和知舞香 <revolution.w88@gmail.com>
2024-01-23 19:12:27 +0900

    記事削除の API とテストの実装

--- spec/models/articles_spec.rb
+++ spec/models/articles_spec.rb
@@ -107,4 +107,34 @@ RSpec.describe "Api::V1::Articles", type: :request do
       end
     end
   end
+
+  describe "DELETE/ articles/:id" do
+    subject { delete(api_v1_article_path(article.id)) }
+
+    let(:current_user) { create(:user) }
+    let(:article_id) { article.id }
+
+    before do
+      allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user)
+    end
+
+    context "任意の記事を削除しようとするとき" do
+      let!(:article) { create(:article, user: current_user) }
+
+      it "任意の記事を削除できる" do
+        expect { subject }.to change { Article.count }.by(-1)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "他人の記事を削除しようとするとき" do
+      let(:other_user) { create(:user) }
+      let!(:article) { create(:article, user: other_user) }
+
+      it "記事を削除できない" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound) &
+                              change { Article.count }.by(0)
+      end
+    end
+  end
 end
